044907a668b66b42752ed66cc00be5e19f2475f4
devnote-dev <inferno0745@gmail.com>
2022-11-13 04:17:24 +0000

    feat(app): style updates, icons & dark mode attempt

--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,9 @@ import {
   Scripts,
   ScrollRestoration,
 } from '@remix-run/react';
+import { useEffect, useState } from 'react';
+import { BsGithub, BsMoonFill, BsSunFill } from 'react-icons/bs';
+import { IoMail } from 'react-icons/io5';
 import styles from './styles/tailwind.css';
 
 export function links() {
@@ -20,6 +23,16 @@ export const meta: MetaFunction = () => ({
 });
 
 export default function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
+    }
+  }, [darkMode]);
+
   return (
     <html lang="en" className="h-full">
       <head>
@@ -27,6 +40,21 @@ export default function App() {
         <Links />
       </head>
       <body className="h-full bg-gradient-to-t from-orange-600 to-white dark:to-black">
+        <nav className="h-12 w-full flex-row items-end justify-between px-4 mt-4">
+          <div className="flex flex-row gap-x-4 text-2xl text-slate-800 dark:bg-white">
+            <a href="mailto:devnote.dev75@gmail.com">
+              <IoMail />
+            </a>
+            <a href="https://github.com/devnote-dev" type="_blank">
+              <BsGithub />
+            </a>
+            {darkMode ? (
+              <BsSunFill onClick={() => setDarkMode(false)} />
+            ) : (
+              <BsMoonFill onClick={() => setDarkMode(true)} />
+            )}
+          </div>
+        </nav>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,5 @@
 export default function Index() {
   return (
-    <div>
-      <h1 className="text-white">Hello world!</h1>
-    </div>
+    <div></div>
   );
 }
