a03a12c4c4be1290f431d3f6d43a5b01a56cc6fe
devnote-dev <inferno0745@gmail.com>
2022-11-13 03:26:15 +0000

    feat(app): basic style changes

--- app/root.tsx
+++ app/root.tsx
@@ -7,21 +7,26 @@ import {
   Scripts,
   ScrollRestoration,
 } from '@remix-run/react';
+import styles from './styles/tailwind.css';
+
+export function links() {
+  return [{ rel: 'stylesheet', href: styles }];
+}
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
-  title: 'New Remix App',
+  title: 'devnote.dev',
   viewport: 'width=device-width,initial-scale=1',
 });
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang="en" className="h-full">
       <head>
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="h-full bg-gradient-to-t from-orange-600 to-white dark:to-black">
         <Outlet />
         <ScrollRestoration />
         <Scripts />
