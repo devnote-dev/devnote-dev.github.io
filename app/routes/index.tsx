ccde181b2ff065b703e00a81cc721c1259d3ab05
devnote-dev <devnote.dev75@gmail.com>
2023-07-18 22:49:51 +0100

    feat(routes): update home page information

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,52 @@
-export default function Index() {
+export default function () {
   return (
-    <div></div>
+    <main>
+      <div className="grid grid-cols-2 gap-x-10 m-12 font-[Roboto Medium,monospace]">
+        <div>
+          <h1 className="mb-2 font-bold text-2xl uppercase">About Me</h1>
+          <p>
+            My name is Devaune Whittle, you may know me online as Devonte or
+            "devnote" (parody name). I am a software developer from the UK
+            working on various open-source and commercial projects which you can
+            find on my GitHub. In my spare time, I'm a hobbyist photographer and
+            athletics enthusiast (although I'm not very good at it). I am also
+            experimenting with videography for personal projects and potential
+            collaborative projects, at some point these will be available on
+            this website too.
+          </p>
+        </div>
+        <div>
+          <h1 className="mb-2 font-bold text-2xl uppercase">Contacts</h1>
+          <ol>
+            <li className="flex flex-wrap">
+              <p className="font-bold uppercase">Gmail •&nbsp;</p>
+              <a
+                className="hover:underline"
+                href="mailto:devnote.dev75@gmail.com"
+              >
+                devnote.dev75@gmail.com
+              </a>
+              &nbsp;(dev)
+            </li>
+            <li className="flex flex-wrap">
+              <p className="font-bold uppercase">Gmail •&nbsp;</p>
+              <a className="hover:underline" href="mailto:dkwhittle4@gmail.com">
+                dkwhittle4@gmail.com
+              </a>
+              &nbsp;(work)
+            </li>
+            <li className="flex flex-wrap">
+              <p className="font-bold uppercase">LinkedIn •&nbsp;</p>
+              <a
+                className="hover:underline"
+                href="https://www.linkedin.com/in/devaune-w-10bb55233"
+              >
+                Devaune W
+              </a>
+            </li>
+          </ol>
+        </div>
+      </div>
+    </main>
   );
 }
