6a5f29874ebb75486c2187c41b229b1c8170e331
devnote-dev <devnote.dev75@gmail.com>
2023-07-18 21:42:30 +0100

    feat: add new navbar

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,35 @@
+import { Link } from '@remix-run/react';
+
+interface Props {
+  home?: boolean;
+  photos?: boolean;
+}
+
+export default function ({ home, photos }: Props) {
+  return (
+    <nav className="w-full h-14 flex flex-wrap items-center justify-center gap-x-6 bg-black text-white shadow-md font-[Roboto Medium,monospace]">
+      {home ? (
+        <div className="decoration-4 underline underline-offset-[17px]">
+          Home
+        </div>
+      ) : (
+        <div className="decoration-4 hover:underline underline-offset-[17px]">
+          <Link to="/">Home</Link>
+        </div>
+      )}
+      {photos ? (
+        <div className="decoration-4 underline underline-offset-[17px]">
+          Photography
+        </div>
+      ) : (
+        <div className="decoration-4 hover:underline underline-offset-[17px]">
+          <Link to="/photos">Photography</Link>
+        </div>
+      )}
+      <div className="decoration-4 hover:underline underline-offset-[17px]">
+        <a href="https://github.com/devnote-dev">GitHub</a>
+      </div>
+      {/* <div className="decoration-4 underline underline-offset-[17px]"><a href="https://linked.in/devaune">LinkedIn</a></div> */}
+    </nav>
+  );
+}
