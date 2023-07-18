9c6ce5a181942c58bc1d05897401c02396b546dc
devnote-dev <devnote.dev75@gmail.com>
2023-07-18 21:42:15 +0100

    chore(*): remove old design

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,3 @@
-import { Tooltip } from '@mui/material';
 import type { MetaFunction } from '@remix-run/node';
 import {
   Links,
@@ -8,9 +7,8 @@ import {
   Scripts,
   ScrollRestoration,
 } from '@remix-run/react';
-import { BsGithub, BsTwitter, BsPaypal } from 'react-icons/bs';
-import { SiGmail } from 'react-icons/si';
 import styles from './styles/tailwind.css';
+import NavBar from './components/NavBar';
 
 export function links() {
   return [{ rel: 'stylesheet', href: styles }];
@@ -21,14 +19,6 @@ export const meta: MetaFunction = () => ({
   title: 'devnote-dev',
   viewport: 'width=device-width,initial-scale=1',
 });
-
-function getColourScheme(): string {
-  let hour = parseInt(new Date().toTimeString().split(':')[0]);
-  if (hour >= 7 && hour < 12) return 'from-[#cdcff9] to-[#ffb885]';
-  if (hour >= 12 && hour < 16) return 'from-[#85c8ff] to-[#494ea7]';
-  if (hour >= 16 && hour < 18) return 'from-[#494ea7] to-[#3b1c59]';
-  return 'from-[#181646] to-[#100f2f]';
-}
 
 export default function App() {
   return (
@@ -43,32 +33,21 @@ export default function App() {
           src="https://your-information.is-on.top/umami.js"
         />
       </head>
-      <body className={`h-screen bg-gradient-to-b ${getColourScheme()}`}>
-        <nav className="w-full px-6 py-2 flex flex-row items-center justify-center gap-x-6 bg-black text-white opacity-50 shadow-lg">
-          <Tooltip title="GitHub">
-            <a href="https://github.com/devnote-dev">
-              <BsGithub className="w-10 h-10" />
-            </a>
-          </Tooltip>
-          <Tooltip title="Gmail (business)">
-            <a href="mailto:dkwhittle4@gmail.com">
-              <SiGmail className="w-11 h-11" />
-            </a>
-          </Tooltip>
-          <Tooltip title="Twitter">
-            <a href="https://twitter.com/devw1_">
-              <BsTwitter className="w-11 h-11" />
-            </a>
-          </Tooltip>
-          <Tooltip title="PayPal">
-            <a href="https://paypal.me/devnote">
-              <BsPaypal className="w-10 h-10" />
-            </a>
-          </Tooltip>
-        </nav>
+      <body className="bg-white">
+        <NavBar home />
         <Outlet />
         <ScrollRestoration />
         <Scripts />
+        <footer className="h-14 w-full bg-black text-center">
+          <div className="flex items-center justify-center">
+            <a
+              className="mt-4 text-gray-300 transition duration-150 ease-in-out hover:text-gray-300"
+              href="https://github.com/devnote-dev"
+            >
+              &copy; 2023 devnote-dev
+            </a>
+          </div>
+        </footer>
         <LiveReload />
       </body>
     </html>
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,48 +1,5 @@
-import { useEffect, useState } from 'react';
-
-function getTextColour(): string {
-  let hour = parseInt(new Date().toTimeString().split(':')[0]);
-  return hour >= 17 || hour < 7 ? 'text-slate-200' : 'text-black';
-}
-
-function getTimeName(): string {
-  let hour = parseInt(new Date().toTimeString().split(':')[0]);
-  if (hour >= 4 && hour < 12) return 'Good Morning';
-  if (hour >= 12 && hour < 18) return 'Good Afternoon';
-  if (hour >= 18 && hour < 22) return 'Good Evening';
-  return '🌙';
-}
-
 export default function Index() {
-  let name = getTimeName();
-  let [time, setTime] = useState(() => {
-    let [hour, minute] = new Date().toTimeString().split(':');
-    return `${hour}:${minute}`;
-  });
-
-  useEffect(() => {
-    setInterval(() => {
-      let [hour, minute] = new Date().toTimeString().split(':');
-      setTime(`${hour}:${minute}`);
-    }, 15_000);
-  });
-
   return (
-    <div
-      className={`flex flex-col justify-center items-center text-center ${getTextColour()}`}
-    >
-      <div className="divide-y">
-        <h1 className="pt-4 pb-2 font-bold text-8xl">{time}</h1>
-        <div className="pt-2 text-xl">{name}</div>
-      </div>
-      <div className="pt-10 mx-4 text-2xl sm:text-lg md:text-xl max-w-4xl">
-        Hi, you probably know me as Devonte online or Devaune in real life, so
-        to simplify things I go by both names. I am an aspiring software
-        developer, hobbyist photographer and psychology enthusiast, but most of
-        the time you will find me programming. I work primarily on the backend
-        of open-source and closed-source projects on Github, and I do private
-        freelance work.
-      </div>
-    </div>
+    <div></div>
   );
 }
