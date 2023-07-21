35109c65b18efb7a13db3b750d425a69c7a414f6
devnote-dev <devnote.dev75@gmail.com>
2023-07-22 00:30:33 +0100

    feat(routes): implement experience page

---
+++ app/routes/experience.tsx
@@ -0,0 +1,74 @@
+import NavBar from '~/components/NavBar';
+
+export default function () {
+  return (
+    <main>
+      <NavBar experience />
+      <div className="grid grid-rows-2 gap-y-10 m-12 font-[Roboto Medium,monospace]">
+        <h1 className="mb-2 font-bold text-4xl uppercase">Experience</h1>
+        <div>
+          <hr className="w-full h-6 rounded-xl bg-[#3572A5]" />
+          <h3 className="ml-4 font-bold text-slate-400 text-xl uppercase">
+            Python
+          </h3>
+        </div>
+        <div>
+          <div className="grid justify-items-end">
+            <hr className="w-[95%] h-6 rounded-xl bg-[#F1E05A]" />
+          </div>
+          <h3 className="ml-[6%] font-bold text-slate-400 text-xl uppercase">
+            JavaScript
+          </h3>
+        </div>
+        <div>
+          <div className="grid justify-items-end">
+            <hr className="w-[90%] h-6 rounded-xl bg-[#3178C6]" />
+          </div>
+          <h3 className="ml-[11%] font-bold text-slate-400 text-xl uppercase">
+            TypeScript
+          </h3>
+        </div>
+        <div>
+          <div className="grid justify-items-end">
+            <hr className="w-[55%] h-6 rounded-xl bg-black" />
+          </div>
+          <h3 className="ml-[46%] font-bold text-slate-400 text-xl uppercase">
+            Crystal
+          </h3>
+        </div>
+        <div>
+          <div className="grid justify-items-end">
+            <hr className="w-[45%] h-6 rounded-xl bg-[#00B4AB]" />
+          </div>
+          <h3 className="ml-[56%] font-bold text-slate-400 text-xl uppercase">
+            Dart
+          </h3>
+        </div>
+        <div>
+          <div className="grid justify-items-end">
+            <hr className="w-[40%] h-6 rounded-xl bg-[#00ADD8]" />
+          </div>
+          <h3 className="ml-[61%] font-bold text-slate-400 text-xl uppercase">
+            Go
+          </h3>
+        </div>
+        <div>
+          <div className="grid justify-items-end">
+            <hr className="w-[25%] h-6 rounded-xl bg-[#DEA584]" />
+          </div>
+          <h3 className="ml-[76%] font-bold text-slate-400 text-xl uppercase">
+            Rust
+          </h3>
+        </div>
+        <div>
+          <div className="grid justify-items-end">
+            <hr className="w-[12%] h-6 rounded-xl bg-[#EC915C]" />
+          </div>
+          <h3 className="ml-[89%] font-bold text-slate-400 text-xl uppercase">
+            Zig
+          </h3>
+        </div>
+      </div>
+    </main>
+  );
+}
