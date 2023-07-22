2307070fee3b6d26dc18c04dc529f69a3cc792ba
devnote-dev <devnote.dev75@gmail.com>
2023-07-22 06:47:03 +0100

    feat(app): implement catch boundary

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,6 @@
 import type { MetaFunction } from '@remix-run/node';
 import {
+  Link,
   Links,
   LiveReload,
   Meta,
@@ -8,6 +9,8 @@ import {
   ScrollRestoration,
 } from '@remix-run/react';
 import styles from './styles/tailwind.css';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 export function links() {
   return [{ rel: 'stylesheet', href: styles }];
@@ -18,6 +21,34 @@ export const meta: MetaFunction = () => ({
   title: 'devnote-dev',
   viewport: 'width=device-width,initial-scale=1',
 });
+
+export function CatchBoundary() {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body className="bg-white">
+        <main className="grid min-h-[100svh] grid-rows-[auto_1fr_auto]">
+          <NavBar />
+          <div className="m-12 font-[Roboto Medium,monospace]">
+            <h1 className="mb-4 font-bold text-4xl uppercase">Oops</h1>
+            <p>
+              Looks like you got lost, maybe you want to&nbsp;
+              <Link className="underline hover:text-cyan-500" to="/">
+                go home
+              </Link>
+              .
+            </p>
+          </div>
+          <Footer />
+        </main>
+        <Scripts />
+      </body>
+    </html>
+  );
+}
 
 export default function App() {
   return (
